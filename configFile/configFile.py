e3ae6be56608742706ebe490358a7b31faf24c8e
Lyh97 <18524637652@163.com>
2018-09-15 16:50:34 +0800

    Add ConfigAPI

--- configFile/configFile.py
+++ configFile/configFile.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Blueprint, request, g, jsonify
+from flask import Blueprint, request, g, jsonify, json
 
 config = Blueprint('configFile', __name__)
 
@@ -13,8 +13,47 @@ def init() :
 @config.route('/configtest', methods=['GET'])
 def Login():
     select_list = []
-    select_result = g.mongo.db.tasklog.find({"taskid":"2036f13c-7ad5-11e8-adbe-0016413cb304"})
-
+    select_result = g.mongo.db.ConfigFile.find({"userid":"1001"})
     for s in select_result:
-        select_list.append({"taskName": s["taskName"], "description": s["description"]})
-    return jsonify({'code': 200, 'meaasge': 'Login Fail', 'data': select_list})
+        select_list.append({"config": s["config"]})
+    return jsonify({'code': 200, 'meaasge': 'dsd', 'data': select_list})
+
+# Select Config By Userid
+@config.route('/selectConfigByUserid',methods=['GET'])
+def selectConfigByUserid():
+    selectResult = []
+    userid = request.args.get('userid')
+
+    if userid:
+        try:
+            selectResult = g.mongo.db.ConfigFile.find_one({"userid":userid})
+        except Exception:
+            return jsonify({'code': 300, 'meaasge': 'Select Fail', 'data': Exception})
+        else:
+            return jsonify({'code': 200, 'meaasge': 'Select Success', 'data': selectResult['config']})
+    else:
+        return jsonify({'code': 301, 'meaasge': 'No UserID', 'data': ''})
+
+# Create a configuration file in the configuration table
+@config.route('/changeconfig', methods=['POST'])
+def changeConfig():
+    userid = request.form.get('userid')
+    configfile = json.loads(request.form.get('config'))
+
+    select_result = g.mongo.db.ConfigFile.find({"userid": userid})
+    print(select_result.count())
+
+    if select_result.count() != 0:
+        try:
+            g.mongo.db.ConfigFile.update({'userid': userid}, { 'userid':userid,'config': configfile})
+        except Exception:
+            return jsonify({'code': 300, 'meaasge': 'Update Fail', 'data': Exception})
+        else:
+            return jsonify({'code': 200, 'meaasge': 'Update Success', 'data': ''})
+    else:
+        try:
+            g.mongo.db.ConfigFile.insert({'userid':userid,'config':configfile})
+        except Exception:
+            return jsonify({'code': 300, 'meaasge': 'Insert Fail', 'data': ''})
+        else:
+            return jsonify({'code': 200, 'meaasge': 'Insert Success', 'data': ''})
